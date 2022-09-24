a17a7792ac5896a7598fe7dc70d251e0165e306c
Eduard-Exuzyan <79186118913@mail.ru>
2022-09-24 20:44:43 +0300

    Initial Commit

---
+++ choiceColorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  choiceColorApp
+//
+//  Created by Эдос Эксузян on 24.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
